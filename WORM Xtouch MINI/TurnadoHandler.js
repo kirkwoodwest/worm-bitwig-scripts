e92ba9cfe21003131350beeb02af3c87c8a94203
Kirkwood West <amtrax@gmail.com>
2020-08-11 20:08:38 -0700

    Turnado Handler Working...

--- WORM Xtouch MINI/TurnadoHandler.js
+++ WORM Xtouch MINI/TurnadoHandler.js
@@ -7,8 +7,16 @@ function TurnadoHandler (track_names, cc_list, hardware) {
    this.hardware = hardware;
    this.enabled_bool = false;
    this.cc_list = cc_list;
+
+
+   //Preset selection
+   this.preset_knob_data = [0,0,0,0,0,0,0,0];
+   this.preset_knobs_pressed = [false,false,false,false,false,false,false];
+   this.preset_edit_all = false;
+   this.preset_edit_knobs = false;
+
    //Hardcoded to one for the first knob (Dictation)
-   var knob_count = 1;
+   var knob_count = 2;
 
    for (var i=0;i<track_names.length;i++){
       var cursor_track_id = "CURSOR_TRACK_TURNADO_" + i;
@@ -58,14 +66,56 @@ TurnadoHandler.prototype.remoteUpdate = function(index, value){
 
 TurnadoHandler.prototype.updateLed = function(){
    if (this.enabled_bool == false) return;
+
+   if( this.preset_edit_knobs){
+      //indicate current preset selection
+     
+      //Light up LED BUTTONS for which presets are selected.
+      for(var i=0;i<this.preset_knobs_pressed.length;i++){
+         var status = 0x90; //note status
+         var note = XTOUCH_LED_KNOBS[0] + i;
+ 
+         note = XTOUCH_BTN_ROW_1[i];
+         var value = 0;
+         if (this.preset_knobs_pressed[i]) value = 127;
+         this.hardware.sendMidi(status, note, value);
+      }
+
+      //Knobs, show values for currently selected preset
+      //Change knob values to match preset first id
+      for(var i=0;i<this.preset_knobs_pressed.length;i++){
+         //Do action based on the first knob that was pressed.
+         if (this.preset_knobs_pressed[i]) {
+            var parameter = this.cursorRemotes[i].getParameter(1); 
+            var preset_value = parameter.getRaw() - 1;
+            
+            //set LED status of knobs
+            for(var d=0;d<XTOUCH_LED_KNOBS_LIST.length;d++){
+               var status = 0xB0; //cc status
+               var cc = XTOUCH_LED_KNOBS_LIST[0] + d;
+               var value = 33;//OFF
+               if (d == preset_value) value = 43; //Full On
+               println('preset value' + preset_value);
+               this.hardware.sendMidi(status, cc, value);
+            }
+            break;
+         }
+      }
+
+
+      //Exit 
+      return;
+   }
+
+   //show values for knobs...
    for (var i = 0; i < this.cursorRemotes.length; i++){
       var parameter = this.cursorRemotes[i].getParameter(0); 
       var value = parameter.get() 
 
       var min_val = 33;
       var max_val = 43;
-      var cc = XTOUCH_LED_KNOBS[0] + i;
-      var status = 0xB0;
+      var cc = XTOUCH_LED_KNOBS_LIST[0] + i;
+      var status = 0xB0;//CC Status
 
       value = map_range(value, 0, 1, min_val, max_val);
       value = Math.round(value);
@@ -76,60 +126,119 @@ TurnadoHandler.prototype.updateLed = function(){
       var status = 0xB0;
    }
 
-      //Update Preset LED
-      //TODO: Implement in bitwig device and make flag for updating this value...
-      for (i = 0; i < this.cursorRemotes.length; i++){
-         var solo_value = false
-         status = 0x90;
-         note = XTOUCH_BTN_ROW_1[i];
-         value = 0;
-         if (solo_value) value = 127;
-         this.hardware.sendMidi(status, note, value);
+   //Update Preset LED
+   //TODO: Implement in bitwig device and make flag for updating this value...
+   for (i = 0; i < this.cursorRemotes.length; i++){
+      var solo_value = false
+      status = 0x90;
+      note = XTOUCH_BTN_ROW_1[i];
+      value = 0;
+      if (solo_value) value = 127;
+      this.hardware.sendMidi(status, note, value);
+   }
+}
+
+TurnadoHandler.prototype.KnobData = function(knob_index){
+   //these are values 0-7 (preset 1 - 8)
+
+}
+
+//Flags which buttons are pressed for editing
+TurnadoHandler.prototype.editTurnadoPreset = function(turnado_id, edit_bool) {
+   this.preset_knobs_pressed[turnado_id] = edit_bool;
+
+   var button_is_pressed = false;
+   for(var i=0;i<this.preset_knobs_pressed.length;i++){
+      if (this.preset_knobs_pressed[i]) {
+         this.preset_edit_knobs = true;
+         return;
       }
-  }
+   }
+
+   //No buttons are pressed.
+   this.preset_edit_knobs = false;
+}
+
+//Selects preset for flagged knobs for edit knobs.
+TurnadoHandler.prototype.presetSelect = function(preset_index) {
+   for(var i=0;i<this.preset_knobs_pressed.length;i++){
+      if (this.preset_knobs_pressed[i]){
+         //send value to turnado...
+         var cursor_remote = this.cursorRemotes[i]; 
+         var parameter = cursor_remote.getParameter(1); 
+         parameter.set(preset_index, 128); 
+      }
+   }
+}
+
 
 //TODO: Convert to using hardware connection...
 TurnadoHandler.prototype.handleMidi = function (status, data1, data2) {
    if (this.enabled_bool == false) return false;
 
+   if (isNoteOn(status) || isNoteOff(status)) {
+      var find_index = XTOUCH_BTN_ROW_1.indexOf(data1);
+      if (find_index != -1){
+         this.preset_knobs_pressed[find_index] = true;
+         var edit = false
+         if (data2 > 64) edit = true;
+         this.editTurnadoPreset(find_index, edit);
+         return;
+      }
+   }
    if (isNoteOn(status) && data2 > 64){
       if (data1 == XTOUCH_BTN_ROW_2[0]) {
-         
          //resets all turnados to 0.
          for (var i = 0; i < this.cursorRemotes.length; i++){
             var cursor_remote = this.cursorRemotes[i];
 
             //Set to point at first parameter of every device. (Dictator)
-            
             var parameter = cursor_remote.getParameter(0); 
             parameter.set(0, 128);    
          }
       } else {
+         //knob presses
+         if ((this.preset_edit_knobs) ) {
+            //Preset Selection mode
+            index = XTOUCH_BTN_KNOBS.indexOf(data1);
 
-         //Reset Specific Turnado Knob to 0.
-         index = XTOUCH_BTN_KNOBS.indexOf(data1);
+            if (index != -1 && index != undefined) {
+               this.presetSelect(index);
+               return true;
+            }
+         } else {
+            //Knob value Reset Mode
+            //Reset Specific Turnado Knob to 0.
+            index = XTOUCH_BTN_KNOBS.indexOf(data1);
 
-         if (index != -1 && index != undefined) {
-            var cursor_remote = this.cursorRemotes[index]; 
-            var parameter = cursor_remote.getParameter(0); 
-            parameter.set(0, 128); 
-            return true;
+            if (index != -1 && index != undefined) {
+               var cursor_remote = this.cursorRemotes[index]; 
+               var parameter = cursor_remote.getParameter(0); 
+               parameter.set(0, 128); 
+               return true;
+            }
          }
       }
    }
 
    if (isChannelController(status)) {
-      var cc = parseInt(data1);
-      var index = this.cc_translation[parseInt(data1)];
+      if (this.preset_edit_knobs) {
+         //do nothing.
 
-      if(index == undefined || index == null) return;
-   
-      if (index != undefined) {
-         var value = data2 > 64 ? 64 - data2 : data2;
-         var cursor_remote = this.cursorRemotes[index]; 
-         var parameter = cursor_remote.getParameter(0); 
-         parameter.inc(value, 128);        
-         return true;
+      } else { 
+         //Control Turnado Dictator
+         var cc = parseInt(data1);
+         var index = this.cc_translation[parseInt(data1)];
+
+         if(index == undefined || index == null) return;
+      
+         if (index != undefined) {
+            var value = data2 > 64 ? 64 - data2 : data2;
+            var cursor_remote = this.cursorRemotes[index]; 
+            var parameter = cursor_remote.getParameter(0); 
+            parameter.inc(value, 128);        
+            return true;
+         }
       }
    }
    return false;    
